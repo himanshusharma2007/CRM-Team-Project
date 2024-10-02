730a56ecb5536e546701e028b52112787abc3ed3
himanshu sharma <sharmahimanshu6478@gmail.com>
2024-10-02 16:51:45 +0530

    auth.js /himanshu

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,44 +1,47 @@
-const jwt = require('jsonwebtoken');
-const user = require('../models/userModels');
+const jwt = require("jsonwebtoken");
+const user = require("../models/userModels");
 
 const jwtToken = async (req, res, next) => {
-  let token = req.cookies.token
-  console.log(token)
+  let token = req.cookies.token;
+  console.log(token);
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_KEY);
-    if(!decoded){
+    if (!decoded) {
       res.status(401).send({
-        message: 'Not authorized, token failed',
-        success: false
-    });
+        message: "Not authorized, token failed",
+        success: false,
+      });
     }
-    req.user = await user.findById(decoded.id).select('-password');
+    req.user = await user.findById(decoded.id).select("-password");
     next();
   } catch (err) {
     res.status(401).send({
-        message: 'Not authorized, token failed',
-        success: false
+      message: "Not authorized, token failed",
+      success: false,
     });
   }
 };
 
-
 const checkAdmin = (req, res, next) => {
-  if (req.user.role !== 'admin') {
+  if (req.user.role !== "admin") {
     return res.status(403).send({
       success: false,
-      message: 'Admin access required'
+      message: "Admin access required",
     });
   }
   next();
 };
 
 const checkSubAdmin = (req, res, next) => {
-  if (req.user.role !== 'devAdmin' || req.user.role !== 'marAdmin' || req.user.role !== 'admin') {
+  if (
+    req.user.role !== "devAdmin" ||
+    req.user.role !== "marAdmin" ||
+    req.user.role !== "admin"
+  ) {
     return res.status(403).send({
       success: false,
-      message: 'Sub-admin access required'
+      message: "Sub-admin access required",
     });
   }
   next();
