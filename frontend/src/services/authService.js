cd181134a1c1562f6e0b36878e4cff4986ef67fc
Dhairya jain <dhairyajain2617@gmail.com>
2024-10-02 16:32:24 +0530

    dhairya pulling

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -85,7 +85,11 @@ export const forgotPassword = async (email) => {
 
 export const verifyOTP = async (email, otp, password) => {
   try {
-    const response = await api.post("/auth/verify-otp", { email, otp, password });
+    const response = await api.post("/auth/verify-otp", {
+      email,
+      otp,
+      password,
+    });
     return response.data;
   } catch (error) {
     console.error(
