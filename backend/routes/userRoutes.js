41cda3e7ab72653c8d38c188b8c27fcc14c3ac4d
ViolentUJJWAL <ujjwal21aman08@gmail.com>
2024-10-07 17:15:11 +0530

    team controller, teamRoutes and verifyUser /ujjwal

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const { getUser, getAllUser } = require("../controller/userController");
-const { jwtToken, checkAdmin } = require("../middleware/auth")
+const { getUser, getAllUser, getUserById, getUnVerifiedUser, verifyUser } = require("../controller/userController");
+const { jwtToken, checkAdmin, checkSubAdmin } = require("../middleware/auth")
 
 
 const routers = express.Router()
 
 routers.get("/", jwtToken, getUser)
 routers.get("/allUsers", jwtToken, checkAdmin, getAllUser)
+routers.get("/unverified", jwtToken, checkAdmin, getUnVerifiedUser)
+routers.get("/:id", jwtToken, checkSubAdmin, getUserById)
+routers.post("/verifyUser", jwtToken, checkAdmin, verifyUser)
 
 module.exports = routers
