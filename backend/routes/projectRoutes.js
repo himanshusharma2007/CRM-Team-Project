59f038178c3e00d26f941674960fb1a1698fe006
ViolentUJJWAL <ujjwal21aman08@gmail.com>
2024-10-16 14:59:42 +0530

    add permission middleware /ujjwal2

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -8,29 +8,18 @@ const {
   updateProjectImage,
   getProjectByLogedInUser,
 } = require("../controller/projectController");
-const { jwtToken, checkAdmin, checkSubAdmin } = require("../middleware/auth");
+const { jwtToken } = require("../middleware/auth");
+const { checkPermission } = require("../middleware/permission")
 const upload = require("../middleware/multer");
 
 const router = express.Router();
 
-router.get("/", jwtToken, checkSubAdmin, getAllProjects);
-router.get("/client/:id", jwtToken, checkSubAdmin, getProjectByClientId);
+router.get("/", jwtToken, checkPermission("project read"), getAllProjects);
+router.get("/client/:id", jwtToken, checkPermission("project read"), getProjectByClientId);
 router.get("/enrolledProjects", jwtToken, getProjectByLogedInUser);
-router.post(
-  "/create",
-  jwtToken,
-  checkAdmin,
-  upload.single("projectImage"),
-  createProject
-);
-router.put("/update/:id", jwtToken, checkSubAdmin, updateProject);
-router.get("/:id", jwtToken, checkSubAdmin, getProjectById);
-router.patch(
-  "/updateImage/:id",
-  jwtToken,
-  checkAdmin,
-  upload.single("projectImage"),
-  updateProjectImage
-);
+router.post("/create",jwtToken,checkPermission("project create"),upload.single("projectImage"),createProject);
+router.put("/update/:id", jwtToken, checkPermission("project update"), updateProject);
+router.get("/:id", jwtToken, checkPermission("project read"), getProjectById);
+router.patch("/updateImage/:id",jwtToken,checkPermission("project update"),upload.single("projectImage"),updateProjectImage);
 
 module.exports = router;
