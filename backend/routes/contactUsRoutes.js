8019be54956f4a05a6935c1d84d893502380b8d3
ViolentUJJWAL <ujjwal21aman08@gmail.com>
2024-10-24 12:06:19 +0530

    checkadmin /ujjwal2

--- backend/routes/contactUsRoutes.js
+++ backend/routes/contactUsRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const { createContactUs, getContactUs, deleteContactUs, responedContactUs } = require("../controller/contactUsController");
 const { jwtToken } = require("../middleware/auth");
 const { checkPermission } = require("../middleware/permission");
-const { checkAdmin } = require("../middleware/checkAdmin");
+const { checkAdmin }  = require("../middleware/checkAdmin")
 
 router.post("/", createContactUs);
 router.get("/all-contactUs", jwtToken, checkPermission("query read"), getContactUs);
-router.delete("/delete-contactUs/:id", jwtToken, checkAdmin, deleteContactUs);
+router.delete("/delete-contactUs/:id", jwtToken, checkAdmin(), deleteContactUs);
 router.put("/responed-contactUs/:id", jwtToken, checkPermission("query respond"), responedContactUs);
 
 module.exports = router;
--- backend/routes/dashboardRoutes.js
+++ backend/routes/dashboardRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { getAdminDashboardData } = require("../controller/dashboardController");
 const { jwtToken } = require("../middleware/auth");
-const { checkAdmin } = require("../middleware/checkAdmin");
+const { checkAdmin } = require("../middleware/checkAdmin")
+
+
 const routers = express.Router();
 
-routers.get("/admin", jwtToken, checkAdmin, getAdminDashboardData);
+routers.get("/admin", jwtToken, checkAdmin(), getAdminDashboardData);
 
 module.exports = routers;
